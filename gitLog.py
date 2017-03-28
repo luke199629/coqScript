a2437e6fdd5df05578571bba6b0d71f5bae1ae7b
luke199629 <luke199629@gmail.com>
2017-03-27 22:12:24 -0500

    added new log file with commit starting from 76d062d5

--- gitLog.py
+++ gitLog.py
@@ -2,18 +2,18 @@
 # @Author: Luke
 # @Date:   2017-03-09 00:57:03
 # @Last Modified by:   luke199629
-# @Last Modified time: 2017-03-17 10:45:20
+# @Last Modified time: 2017-03-27 22:10:46
 import git, os, shutil
 import subprocess
 import time
 
-fileName = "../coqScript/gitLog.txt"
+fileName = "newestLog.txt"
 curtime = time.time()
 command = "cd .. && cd UniMath && git log | tee "+fileName
 print(subprocess.call([command], shell=True))
 print(time.time() - curtime)
 
-file = open("gitLog.txt")
+file = open("newestLog.txt")
 
 commits = []
 commitHist = open("commitHist.txt", "w")
